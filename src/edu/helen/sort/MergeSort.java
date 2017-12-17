47f917842fac8e6cf029a94e16f8d493305c22c1
helenhan <helenhan27@gmail.com>
2017-12-16 20:03:20 -0500

    update leetcode code

--- src/edu/helen/sort/MergeSort.java
+++ src/edu/helen/sort/MergeSort.java
@@ -46,9 +46,42 @@ public class MergeSort {
 		int a[] = { 49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 5, 4, 62,
 				99, 98, 54, 56, 17, 18, 23, 34, 15, 35, 25, 53, 51 };
 		MergeSort sort = new MergeSort();
-		sort.mergeSort(a, 0, a.length - 1);
+		sort.mergeSort2(a, 0, a.length - 1);
 		String str = Arrays.toString(a);
 		System.out.println(str);
+	}
+
+	public void mergeSort2(int[] num, int low, int high) {
+		if(low<high){
+			int mid = low + (high-low)/2;
+			mergeSort2(num,low,mid);
+			mergeSort2(num,mid+1,high);
+			merge2(num,low,mid,high);
+		}
+	}
+
+	private void merge2(int[] nums,int low, int mid, int high){
+		int[] help = new int[nums.length];
+		for(int i=0;i<nums.length;i++){
+			help[i] = nums[i];
+		}
+		int left =low;
+		int right = mid+1;
+		int k = low;
+		while(left<=mid&&right<=high){
+			if(help[left]<=help[right]){
+				nums[k++] = help[left++];
+			}else{
+				nums[k++] = help[right++];
+			}
+		}
+		while(left<=mid){
+			nums[k++] = help[left++];
+		}
+		while(right<=high){
+			nums[k++] = help[right++];
+		}
+
 	}
 
 }
--- src/edu/helen/sort/QuickSort.java
+++ src/edu/helen/sort/QuickSort.java
@@ -2,108 +2,77 @@ package edu.helen.sort;
 
 import java.util.Arrays;
 
+/**
+ * quick VS merge
+ * <p>
+ * time                                        space                稳定性                其它
+ * <p>
+ * quick   O(nlogn)平均,慢会O(n^2)          O(1)                 不稳定             先整体后局部
+ * <p>
+ * merge    一直O(nlogn)                             O(n)                  稳定               先局部后整体
+ * <p>
+ * merge sort因为要重新开一个数组，开空间再删空间这个操作影响了速度， 所以quick sort是比merge sort快的
+ */
 public class QuickSort {
+    public void quickSort(int[] num) {
+        if (num == null || num.length == 0) {
+            return;
+        }
+        quickSort(num, 0, num.length - 1);
+    }
 
-	public QuickSort() {
-	}
+    public static void main(String[] args) {
+        int[] x = {9, 2, 4, 7, 3, 7, 10};
+        System.out.println(Arrays.toString(x));
 
-	public void quickSort(int[] num) {
-		if (num == null || num.length == 0) {
-			return;
-		}
-		sort(num, 0, num.length - 1);
-	}
+        int low = 0;
+        int high = x.length - 1;
 
-	private void sort(int[] num, int low, int high) {
-		int i = low;
-		int j = high;
-		int pivot = num[low + (high - low) / 2];
-		while (i <= j) {
-			while (num[i] < pivot) {
-				i++;
-			}
-			while (num[j] > pivot) {
-				j--;
-			}
-			// If we have found a values in the left list which is larger then
-			// the pivot element and if we have found a value in the right list
-			// which is smaller then the pivot element then we exchange the
-			// values.
-			// As we are done we can increase i and j
-			if (i <= j) {
-				int temp = num[i];
-				num[i] = num[j];
-				num[j] = temp;
-				i++;
-				j--;
-			}
-		}
-		if (j > low) {
-			sort(num, low, j);
-		}
-		if (i < high) {
-			sort(num, i, high);
-		}
-	}
+        quickSort(x, low, high);
+        System.out.println(Arrays.toString(x));
+    }
 
-//	public static void main(String[] args) {
-////		int a[] = { 49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 5, 4, 62,
-////				99, 98, 54, 56, 17, 18, 23, 34, 15, 35, 25, 53, 51 };
-//		int a [] = {3,4,2,5,1};
-//		QuickSort sort = new QuickSort();
-//		sort.quickSort(a);
-//		String str = Arrays.toString(a);
-//		System.out.println(str);
-//	}
+    public static void quickSort(int[] arr, int low, int high) {
+        if (arr == null || arr.length == 0)
+            return;
 
-	public static void main(String[] args) {
-		int[] x = { 9, 2, 4, 7, 3, 7, 10 };
-		System.out.println(Arrays.toString(x));
+        if (low >= high)
+            return;
 
-		int low = 0;
-		int high = x.length - 1;
+        // pick the pivot
+        int middle = low + (high - low) / 2;
+        // key point 1: pivot is the value, not the index
+        int pivot = arr[middle];
 
-		quickSort(x, low, high);
-		System.out.println(Arrays.toString(x));
-	}
+        // make left < pivot and right > pivot
+        int i = low, j = high;
+        // key point 2: every time you compare left & right, it should be
+        // left <= right not left < right
+        while (i <= j) {
+            // key point 3: A[left] < pivot not A[left] <= pivot
+            while (i <= j && arr[i] < pivot) {
+                i++;
+            }
+            // key point 3: A[right] > pivot not A[right] >= pivot
+            while (i <= j && arr[j] > pivot) {
+                j--;
+            }
 
-	public static void quickSort(int[] arr, int low, int high) {
-		if (arr == null || arr.length == 0)
-			return;
+            if (i <= j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
 
-		if (low >= high)
-			return;
+        // recursively sort two sub parts
+        if (low < j)
+            quickSort(arr, low, j);
 
-		// pick the pivot
-		int middle = low + (high - low) / 2;
-		int pivot = arr[middle];
-
-		// make left < pivot and right > pivot
-		int i = low, j = high;
-		while (i <= j) {
-			while (arr[i] < pivot) {
-				i++;
-			}
-
-			while (arr[j] > pivot) {
-				j--;
-			}
-
-			if (i <= j) {
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-				i++;
-				j--;
-			}
-		}
-
-		// recursively sort two sub parts
-		if (low < j)
-			quickSort(arr, low, j);
-
-		if (high > i)
-			quickSort(arr, i, high);
-	}
+        if (high > i)
+            quickSort(arr, i, high);
+    }
 
 }
